f9c3cad521c858816fab2cac9c80106cf0821c7e
Hiroki <hirokitomita710@gmail.com>
2019-09-06 04:29:07 +0900

    完成

--- js/rader_chart.js
+++ js/rader_chart.js
@@ -1,3 +1,4 @@
+// ①想吃担担面のレーダーチャート
 $(function(){
 
   // 色の設定
@@ -27,17 +28,17 @@ $(function(){
   	data: {
   		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
   		datasets: [{
-  			label: "ラーメン申家",
+  			label: "想吃担担面",
   			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
   			borderColor: colorSet.red,
   			pointBackgroundColor: colorSet.red,
-  			data: [4, 3, 5, 3, 3]
+  			data: [4, 2, 2, 4, 2]
   		},{
   			label: "平均",
   			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
   			borderColor: colorSet.blue,
   			pointBackgroundColor: colorSet.blue,
-  			data: [3, 3, 3, 3, 3]
+  			data: [2, 2, 2, 2, 2]
   		},]
   	},
   	options: {
@@ -75,81 +76,753 @@ $(function(){
   /*
    * チャートの作成
    */
-  var myRadar = new Chart($("#myChart"), config);
+  var myRadar = new Chart($("#myChart1"), config);
+
+});
+
+
+// ②日の出らーめんのレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
 
   /*
-   * ランダムデータ（#randomizeData）
+   * チャートの初期設定
    */
-  $("#randomizeData").click(function(){
-  	config.data.datasets.forEach(function(dataset){
-  		dataset.data = dataset.data.map(function(){
-  			return rnd100();
-  		});
-  	});
-
-  	myRadar.update();
-  });
-
-  var colorNames = Object.keys(colorSet);
-
-  /*
-   * データセットの追加（#addDataset）
-   */
-  $("#addDataset").click(function(){
-  	var colorName = colorNames[config.data.datasets.length % colorNames.length];;
-  	var newColor = colorSet[colorName];
-
-  	var newDataset = {
-  		label: 'Dataset ' + config.data.datasets.length,
-  		borderColor: newColor,
-  		backgroundColor: color(newColor).alpha(0.2).rgbString(),
-  		pointBorderColor: newColor,
-  		data: [],
-  	};
-
-  	for (var index=0; index < config.data.labels.length; ++index) {
-  		newDataset.data.push(rnd100());
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "日の出らーめん",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [3, 3, 3, 3, 4]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
   	}
-
-  	config.data.datasets.push(newDataset);
-  	myRadar.update();
-  });
+  };
 
   /*
-   * データの追加（#addData）
+   * チャートの作成
    */
-  $("#addData").click(function(){
-  	if (config.data.datasets.length > 0){
-  		config.data.labels.push('dataset #' + config.data.labels.length);
+  var myRadar = new Chart($("#myChart2"), config);
 
-  		config.data.datasets.forEach(function(dataset){
-  			dataset.data.push(rnd100());
-  		});
+});
 
-  		myRadar.update();
+
+// ③らーめん まぜそば あらしのレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "らーめん まぜそば あらし",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [3, 3, 3, 2, 3]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
   	}
-  });
+  };
 
   /*
-   * データセットの削除（#removeDataset）
+   * チャートの作成
    */
-  $("#removeDataset").click(function(){
-  	config.data.datasets.splice(0, 1);
-  	myRadar.update();
-  });
+  var myRadar = new Chart($("#myChart3"), config);
+
+});
+
+
+// ④油そば 春日亭のレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
 
   /*
-   * データの削除（#removeData）
+   * チャートの初期設定
    */
-  $("#removeData").click(function(){
-  	config.data.labels.pop(); // remove the label first
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "油そば 春日亭",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [4, 4, 4, 2, 3]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
 
-  	config.data.datasets.forEach(function(dataset){
-  		dataset.data.pop();
-  	});
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart4"), config);
 
-  	myRadar.update();
-  });
+});
 
+
+// ⑤横浜家系ラーメン ぎん家のレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "横浜家系ラーメン ぎん家",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [3, 3, 2, 3, 3]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
+
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart5"), config);
+
+});
+
+
+// ⑥にぼしらーめん88のレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "にぼしらーめん88",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [5, 4, 2, 2, 2]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
+
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart6"), config);
+
+});
+
+
+// ⑦R（アール）のレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "R（アール）",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [5, 2, 2, 2, 5]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
+
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart7"), config);
+
+});
+
+
+// ⑧ラーメン 申家のレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "ラーメン申家",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [4, 3, 4, 4, 2]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
+
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart8"), config);
+
+});
+
+
+// ⑨杏亭のレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "杏亭",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [4, 3, 2, 3, 2]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
+
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart9"), config);
+
+});
+
+
+// ⑩麺屋はやぶさのレーダーチャート
+$(function(){
+
+  // 色の設定
+  var colorSet = {
+  	red: 'rgb(255, 99, 132)',
+  	orange: 'rgb(255, 159, 64)',
+  	yellow: 'rgb(255, 205, 86)',
+  	green: 'rgb(75, 192, 192)',
+  	blue: 'rgb(54, 162, 235)',
+  	purple: 'rgb(153, 102, 255)',
+  	grey: 'rgb(201, 203, 207)'
+  };
+
+  // 乱数生成(0～100)
+  var rnd100 = function(){
+  	return Math.round(Math.random() * 100);
+  };
+
+  // 色のRGB変換
+  var color = Chart.helpers.color;
+
+  /*
+   * チャートの初期設定
+   */
+  var config = {
+  	type: 'radar',
+  	data: {
+  		labels: ["個性", "価格(安)", "ラーメンの種類", "サイドメニュー", "ボリューム"],
+  		datasets: [{
+  			label: "麺屋はやぶさ",
+  			backgroundColor: color(colorSet.red).alpha(0.5).rgbString(),
+  			borderColor: colorSet.red,
+  			pointBackgroundColor: colorSet.red,
+  			data: [5, 2, 4, 2, 3]
+  		},{
+  			label: "平均",
+  			backgroundColor: color(colorSet.blue).alpha(0.5).rgbString(),
+  			borderColor: colorSet.blue,
+  			pointBackgroundColor: colorSet.blue,
+  			data: [2, 2, 2, 2, 2]
+  		},]
+  	},
+  	options: {
+  		animation:false,
+  		showTooltips: false,
+  		legend: { position: 'bottom' },
+  		title: {
+  			display: true,
+  			fontSize:20,
+  			fontColor:'#666',
+  			// text: 'ラーメンチャート'
+  		},
+  		scale: {
+  			display: true,
+  			pointLabels: {
+  				fontSize: 15,
+  				fontColor: colorSet.yellow
+  			},
+  			ticks: {
+  				display: true,
+  				fontSize: 12,
+  				fontColor: colorSet.green,
+  				min: 0,
+  				max: 5,
+  				beginAtZero: true
+  			},
+  			gridLines: {
+  				display: true,
+  				color: colorSet.yellow
+  			}
+  		}
+  	}
+  };
+
+  /*
+   * チャートの作成
+   */
+  var myRadar = new Chart($("#myChart10"), config);
 
 });
